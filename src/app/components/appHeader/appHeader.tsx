41d9cdf9c10f1335eabaa410cdfd4c5a434a9698
JC <jcsmileyjr@gmail.com>
2024-04-15 15:48:54 -0500

    Feature - initial AppHeader component and test

---
+++ src/app/components/appHeader/appHeader.tsx
@@ -0,0 +1,9 @@
+export default function AppHeader() {
+    return (
+        <header className="">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
+                S & F Maintenance Assistant
+            </h1>
+        </header>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
-import Dashboard from './dashboard/page'
+import Dashboard from './dashboard/page';
+import AppHeader from './components/appHeader/appHeader';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col p-1 bg-white">
+      <AppHeader />
       <Dashboard />
     </div>
   )
