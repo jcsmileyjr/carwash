d3061cb14c8119232ac2710603186ebd38996886
JC <jcsmileyjr@gmail.com>
2024-04-17 04:35:44 -0500

    Refactor Issue component: Initial details & summary update

--- src/app/components/issues/issues.tsx
+++ src/app/components/issues/issues.tsx
@@ -9,19 +9,31 @@ import LightBulb from '../../../images/bulb-icon-yellow.png';
  * @param {Issue} issue.issue - The issue data.
  * @return {JSX.Element} The rendered list of issues.
  */
-const Issues = ({issue}: {issue: Issue}) => {
+const Issues = ({issue, summaryList}: {issue: Issue, summaryList: boolean}) => {
     return (
         <div className='mx-12'>
             {Object.keys(issue).map((key) => {
-                if(key === 'pickTrash' || key === 'cleanVacuum' || key === 'dumpTrashCans' || key === 'mowLawn' || key === 'checkVacuumHoses' || key === 'checkBayEquipment') {
-                    const action = issue[key];
-                    return (
-                        <div className='flex flex-row justify-start gap-4 my-2' key={key}>
-                            <Image priority={false} src={LightBulb} width={20} height={5} alt="" className='h-6' />
-                            <p>{action.content}</p>
-                        </div>
-                    )
-                }
+                if (summaryList === true) {
+                    if (key === 'pickTrash' || key === 'cleanVacuum' || key === 'dumpTrashCans' || key === 'checkBayEquipment') {
+                        const action = issue[key];
+                        return (
+                            <div className='flex flex-row justify-start gap-4 my-2' key={key}>
+                                <Image priority={false} src={LightBulb} width={20} height={5} alt="" className='h-6' />
+                                <p>{action.content}</p>
+                            </div>
+                        )
+                    }    
+                } else if (summaryList === false) {
+                    if (key === 'mowLawn' || key === 'checkBayEquipment') {                        
+                        const action = issue[key];
+                        return (
+                            <div className='flex flex-row justify-start gap-4 my-2' key={key}>
+                                <Image priority={false} src={LightBulb} width={20} height={5} alt="" className='h-6' />
+                                <p>{action.content}</p>
+                            </div>
+                        )
+                    }
+                }               
             })}
         </div>
     )
--- src/app/components/whatToDo/whatToDo.tsx
+++ src/app/components/whatToDo/whatToDo.tsx
@@ -6,14 +6,17 @@ import Issues from '../issues/issues';
 export default function WhatToDo() {
     return (
         <section className="flex flex-col border-dashed border-t-2 border-slate-500">
-            <div>
-                <h1 className='text-xl text-center font-bold mt-4'>What To Do</h1>
-            </div>
-            <div className='flex justify-center gap-2'>
-                <Image priority={false} src={QuestionMark} width={10} height={5} alt="" className='' />
-                <p className='text-xs'>Expand to see All</p>
-            </div>
-            <Issues issue={WhatToDoIssue}  />
+            <details>
+                <summary>
+                    <h1 className='text-xl text-center font-bold mt-4'>What To Do</h1>
+                    <div className='flex justify-center gap-2'>
+                        <Image priority={false} src={QuestionMark} width={10} height={5} alt="" className='' />
+                        <p className='text-xs'>Expand to see All</p>
+                    </div>
+                    <Issues issue={WhatToDoIssue} summaryList={true} />
+                </summary>
+                <Issues issue={WhatToDoIssue} summaryList={false}  />
+            </details>
         </section>
     )
 }
