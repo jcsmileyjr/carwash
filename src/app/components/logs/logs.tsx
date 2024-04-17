b04592a9341fd3ac5b72ebe62b603f62b6b9c56d
JC <jcsmileyjr@gmail.com>
2024-04-17 14:56:21 -0500

    Refactor to limit issues in Logs to 30 only
    Also remove the question mark image in two components

--- src/app/components/logs/logs.tsx
+++ src/app/components/logs/logs.tsx
@@ -1,34 +1,50 @@
 
 
-import Image from 'next/image';
-import QuestionMark from '../../../images/question-mark-icon.png';
-import CaretUp from '../../../images/caret-up-icon.png';
-import CaretDown from '../../../images/caret-down-icon.png';
-import Issues from '../issues/issues';
+// import Image from 'next/image';
+// import QuestionMark from '../../../images/question-mark-icon.png';
 import Issue from '../../Types/issue';
 import MaintenanceToggle from '../maintenanceToggle/maintenanceToggle';
 
+/**
+ * Sorts an array of Issue objects by their issueID property in descending order.
+ * If the sorted array has more than 30 elements, it returns a slice of the first 30 elements.
+ *
+ * @param {Issue[]} logs - The array of Issue objects to be sorted.
+ * @return {Issue[]} - The sorted array of Issue objects, or a slice of the first 30 elements if the sorted array has more than 30 elements.
+ */
 const sortIssuesByIssueID = (logs : Issue[]) => {
-    return logs.sort((a, b) => b.issueID - a.issueID);
+    let sortedLogs = logs.sort((a, b) => b.issueID - a.issueID)
+    if (sortedLogs.length > 30) {
+        return sortedLogs.slice(0,30);
+    } else {
+        return sortedLogs;
+    }
 }
 
+/**
+ * Renders the Logs component that displays the sorted maintenance logs.
+ *
+ * @param {Object} props - The props object.
+ * @param {Issue[]} props.maintenanceLogs - The array of maintenance logs to be sorted.
+ * @return {JSX.Element} The rendered Logs component.
+ */
 const Logs = ({maintenanceLogs} : {maintenanceLogs: Issue[]}) => {
-
     const sortedMaintenanceLogs = sortIssuesByIssueID(maintenanceLogs);
 
     return (
-        <section className="flex flex-col border-dashed border-t-2 border-slate-500">
-            <h1 className='text-xl font-bold text-center'> Maintenance Logs </h1>
+        <section className="flex flex-col border-dashed border-t-2 border-slate-500 mx-12">
+            <h1 className='text-xl font-bold text-center mt-4'> Maintenance Logs </h1>
             <div className='flex justify-center gap-2'>
-                <Image priority={false} src={QuestionMark} width={10} height={5} alt="" className='' />
-                <p className='text-xs'>Expand to see last 30 logs</p>
+                {/* <Image priority={false} src={QuestionMark} width={10} height={5} alt="" className='' /> */}
+                <p className='text-xs'>Last 30 logs</p>
             </div>
-            <MaintenanceToggle issue={sortedMaintenanceLogs[0]} />
-            {/* <Issues issue={collapseIssue} imageType='lightbulb' /> */}
+            {
+                sortedMaintenanceLogs.map((issue) => {
+                    return <MaintenanceToggle key={issue.issueID} issue={issue} />;
+                })
+            }
         </section>
     );
 }
-
-// numArray.sort((a, b) => b - a);
 
 export default Logs;
