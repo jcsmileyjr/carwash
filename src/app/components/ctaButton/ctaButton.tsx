114241b2d74ed39fe74d1a50d08c5ae12ddeb9b5
JC <jcsmileyjr@gmail.com>
2024-04-17 12:13:23 -0500

    Styling: Box shadow below CTAbuttons

--- src/app/components/ctaButton/ctaButton.tsx
+++ src/app/components/ctaButton/ctaButton.tsx
@@ -1,7 +1,7 @@
 
 const CTAButton = ({content}:{content: string}) => {
     return (
-        <button type="button" className="inline-flex items-center justify-center rounded-md border border-transparent bg-goldYellow hover:bg-slate-900 px-5 py-3 text-base font-medium text-black hover:text-goldYellow w-1/2 mx-auto">
+        <button type="button" className="inline-flex items-center justify-center rounded-md border border-transparent bg-goldYellow hover:bg-slate-900 px-5 py-3 text-base font-medium text-black hover:text-goldYellow w-1/2 mx-auto shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
             {content}
         </button>
     );
