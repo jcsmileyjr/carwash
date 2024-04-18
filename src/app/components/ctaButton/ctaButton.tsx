411a5031f8936334213143e85a3d1c9ac2a32b03
JC <jcsmileyjr@gmail.com>
2024-04-18 08:35:13 -0500

    Refactor buttons to link to Maintenance page
    Also update tests

--- src/app/components/ctaButton/ctaButton.tsx
+++ src/app/components/ctaButton/ctaButton.tsx
@@ -1,9 +1,9 @@
-
-const CTAButton = ({content}:{content: string}) => {
+import Link from 'next/link';
+const CTAButton = ({content, route}:{content: string, route: string}) => {
     return (
-        <button type="button" className="inline-flex items-center justify-center rounded-md border border-transparent bg-goldYellow hover:bg-slate-900 px-5 py-3 text-base font-medium text-black hover:text-goldYellow w-1/2 mx-auto shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+        <Link href={route} className="inline-flex items-center justify-center rounded-md border border-transparent bg-goldYellow hover:bg-slate-900 px-5 py-3 text-base font-medium text-black hover:text-goldYellow w-1/2 mx-auto shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
             {content}
-        </button>
+        </Link>
     );
 }
 
