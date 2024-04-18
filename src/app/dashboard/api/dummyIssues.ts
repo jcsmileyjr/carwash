57f6e310108a83f8e609b5871a56eb574f4f891c
JC <jcsmileyjr@gmail.com>
2024-04-18 09:31:09 -0500

    Setup rout to send Dummy data

---
+++ src/app/dashboard/api/dummyIssues.ts
@@ -0,0 +1,122 @@
+export const DummyIssues = [
+    {
+        "issueID": 1,
+        "date": "2024-04-10",
+        "logType": "action",
+        "content": null,
+        "pickTrash": {
+                "content": "Pick up trash",
+                "checked": true
+        },
+        "cleanVacuum": {
+            "content": "Clean out bottom of vaccum",
+            "checked": true
+        },
+        "dumpTrashCans": {
+                "content": "Dump trash cans",
+                "checked": true
+        },
+        "mowLawn": {
+                "content": "Cut or weed the lawn",
+                "checked": false
+        },
+       "checkVacuumHoses": {
+                "content": "Check vacuums hose’s heads for obstructions",
+                "checked": false
+        },
+        "checkBayEquipment": {
+                "content": "Check each bay wand or brush to ensure it works.",
+                "checked": false
+        }
+    },
+    {
+        "issueID": 2,
+        "date": "2024-04-11",
+        "logType": "action",
+        "content": null,
+        "pickTrash": {
+                "content": "Pick up trash",
+                "checked": true
+        },
+        "cleanVacuum": {
+            "content": "Clean out bottom of vaccum",
+            "checked": false
+        },
+        "dumpTrashCans": {
+                "content": "Dump trash cans",
+                "checked": false
+        },
+        "mowLawn": {
+                "content": "Cut or weed the lawn",
+                "checked": false
+        },
+       "checkVacuumHoses": {
+                "content": "Check vacuums hose’s heads for obstructions",
+                "checked": false
+        },
+        "checkBayEquipment": {
+                "content": "Check each bay wand or brush to ensure it works.",
+                "checked": true
+        }
+    },
+    {
+        "issueID": 3,
+        "date": "2024-04-13",
+        "logType": "action",
+        "content": null,
+        "pickTrash": {
+                "content": "Pick up trash",
+                "checked": false
+        },
+        "cleanVacuum": {
+            "content": "Clean out bottom of vaccum",
+            "checked": false
+        },
+        "dumpTrashCans": {
+                "content": "Dump trash cans",
+                "checked": false
+        },
+        "mowLawn": {
+                "content": "Cut or weed the lawn",
+                "checked": true
+        },
+       "checkVacuumHoses": {
+                "content": "Check vacuums hose’s heads for obstructions",
+                "checked": false
+        },
+        "checkBayEquipment": {
+                "content": "Check each bay wand or brush to ensure it works.",
+                "checked": false
+        }
+    },
+    {
+        "issueID": 4,
+        "date": "2024-04-14",
+        "logType": "maintenance",
+        "content": "Fixed bay two motor not working. Clear debris from moter caps.",
+        "pickTrash": {
+                "content": "Pick up trash",
+                "checked": false
+        },
+        "cleanVacuum": {
+            "content": "Clean out bottom of vaccum",
+            "checked": false
+        },
+        "dumpTrashCans": {
+                "content": "Dump trash cans",
+                "checked": false
+        },
+        "mowLawn": {
+                "content": "Cut or weed the lawn",
+                "checked": false
+        },
+       "checkVacuumHoses": {
+                "content": "Check vacuums hose’s heads for obstructions",
+                "checked": false
+        },
+        "checkBayEquipment": {
+                "content": "Check each bay wand or brush to ensure it works.",
+                "checked": false
+        }
+    }        
+]
