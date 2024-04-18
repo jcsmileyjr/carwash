f1c93f0e39884644ba8ddd49535dcc47e698ed0d
JC <jcsmileyjr@gmail.com>
2024-04-18 15:32:11 -0500

    Refactor Dashboard to get data from api server route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,26 @@
 import CTAGroup from '../components/ctaGroup/ctaGroup';
 import WhatToDo from '../components/whatToDo/whatToDo';
 import Logs from '../components/logs/logs';
-import DummyIssues from '../lib/dummyIssues.json';
+// import DummyIssues from '../lib/dummyIssues.json';
+import Issue from '../Types/issue';
 
-export default function Dashboard() {
-    return(
+async function getMaintenanceData () {
+    const response = await fetch('http://localhost:3000/dashboard/api');  
+    const data = await response.json();  
+    return data;
+}
+
+export default async function Dashboard() { 
+    const maintenanceData = await getMaintenanceData() as Issue[];
+    return (
         <main className='md:w-1/2 md:mx-auto'>
             <CTAGroup />
             <WhatToDo />
-            <Logs maintenanceLogs={DummyIssues} />
+            <Logs maintenanceLogs={maintenanceData} />
         </main>
-    )
+    );
 }
+
 
 // Notes
 // export default function Dashboard({WhatToDoIssue}: {WhatToDoIssue: IssueType}) {
