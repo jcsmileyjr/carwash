fcb86cb3b75ecaf5eb57e4f84b8dd4757f414d67
JC <jcsmileyjr@gmail.com>
2024-04-22 07:50:42 -0500

    Refactor to send user input content to the Dashboard page

--- src/app/foundIssue/page.tsx
+++ src/app/foundIssue/page.tsx
@@ -4,11 +4,24 @@ import CTAButton from "../components/ctaButton/ctaButton";
 import BackButton from "../components/backButton/backButton";
 import Image from 'next/image';
 import Calendar from '../../images/calendar.png';
+import WhatToDoIssue from '../lib/whatToDoIssue.json';
 import { useState } from 'react';
 import moment from 'moment'; // NPM module that converts date objects to strings
+import {v4 as uuidv4} from 'uuid'; // NPM module that creates a random ID number
 
 export default function FoundIssue() {
-    const [date, setDate] = useState(moment().format("YYYY-MM-DD"));
+    const [date, setDate] = useState(moment().format("YYYY-MM-DD")); 
+    const [content, setContent] = useState("");
+
+    // TODO: Create a function that creates a default issue object with the specified current date.
+    const handleSubmit = () => {
+        let defaultIssue = JSON.parse(JSON.stringify(WhatToDoIssue));
+        defaultIssue.date = date;
+        defaultIssue.issueID = uuidv4();
+        defaultIssue.content = content;
+        defaultIssue.logType = 'maintenance';
+        return JSON.stringify(defaultIssue);
+    }
 
     return (
         <main className="md:w-1/2 md:mx-auto flex min-h-screen flex-col p-1 bg-white">
@@ -19,10 +32,10 @@ export default function FoundIssue() {
                     <Image priority={false} src={Calendar} width={40} height={30} alt="" className='h-8 mr-4 self-end' />
                     <input aria-label="Date" type="date" value={date} onChange={(e) => setDate(e.target.value)}  className="border-b-4 border-black w-full"/>
                 </div>
-                <textarea cols={40} rows={15} className="border-4 border-black" placeholder="Describe the problem in 100 characters or less. " />
+                <textarea value={content} onChange={(e) => setContent(e.target.value)} cols={40} rows={15} className="border-4 border-black" placeholder="Describe the problem in 100 characters or less. " />
             </div>
-            <CTAButton content="Save" route="/" />
-            <BackButton content="Don't Save, Go Back" route="/"/>
+            <CTAButton content="Save" route="/" appData={handleSubmit()} />
+            <BackButton content="Don't Save, Go Back" route="/" />
         </main>
     )
 };
