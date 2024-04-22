d37a3250c74365871a1430da8efd4359871efa0c
JC <jcsmileyjr@gmail.com>
2024-04-22 15:50:31 -0500

    Bug fix for server issue
    The API call is calling the localhost.

--- src/app/utils/getMaintenanceData.ts
+++ src/app/utils/getMaintenanceData.ts
@@ -1,6 +1,7 @@
-
+import {DummyIssues} from '../dashboard/api/dummyIssues';
 export default async function getMaintenanceData () {
-    const response = await fetch('http://localhost:3000/dashboard/api');  
-    const data = await response.json();  
-    return data;
+    return DummyIssues;
+    // const response = await fetch('http://localhost:3000/dashboard/api');  
+    // const data = await response.json();  
+    //return data;
 }
