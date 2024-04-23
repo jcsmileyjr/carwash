e4a539b6445ba35d91d9fe0bdeec392f7ac5ede7
JC <jcsmileyjr@gmail.com>
2024-04-23 08:03:58 -0500

    UI Bug - production app text-area text is white on white background

--- src/app/foundIssue/page.tsx
+++ src/app/foundIssue/page.tsx
@@ -33,7 +33,7 @@ export default function FoundIssue() {
                     <Image priority={false} src={Calendar} width={40} height={30} alt="" className='h-8 mr-4 self-end' />
                     <input aria-label="Date" type="date" value={date} onChange={(e) => setDate(e.target.value)}  className="border-b-4 border-black w-full bg-white text-black"/>
                 </div>
-                <textarea value={content} onChange={(e) => setContent(e.target.value)} cols={40} rows={15} className="border-4 border-black" placeholder="Describe the problem in 100 characters or less. " />
+                <textarea value={content} onChange={(e) => setContent(e.target.value)} cols={40} rows={15} className="border-4 border-black text-black" placeholder="Describe the problem in 100 characters or less. " />
             </div>
             <CTAButton content="Save" route="/" appData={handleSubmit()} />
             <BackButton content="Don't Save, Go Back" route="/" />
