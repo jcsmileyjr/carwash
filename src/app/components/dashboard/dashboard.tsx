c4ed36e81362487fbb631ab514415c91e1bcb319
JC <jcsmileyjr@gmail.com>
2024-05-04 05:22:46 -0500

    Connected a Sanity.io backend to the app
    Use an .env file to save credentials and called data from within the page instead of out.

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -2,7 +2,8 @@ import CTAGroup from '../ctaGroup/ctaGroup';
 import WhatToDo from '../whatToDo/whatToDo';
 import Logs from '../logs/logs';
 import Issue from '../../Types/issue';
-import getMaintenanceData from '../../utils/getMaintenanceData';
+//import getMaintenanceData from '../../utils/getMaintenanceData';
+import {createClient} from '@sanity/client';
 
 /**
  * Checks if an issue is a duplicate by comparing its issueID with the issueIDs of all the issues in the issueArray.
@@ -15,8 +16,26 @@ const isDuplicateIssue = (issue: Issue, issueArray: Issue[]) => {
     return issueArray.some((existingIssue) => existingIssue.issueID === issue.issueID);
 }
 
+const client = createClient({
+    projectId: process.env.LOCAL_PROJECT_ID,
+    dataset: 'production',
+    useCdn: true, // set to `false` to bypass the edge cache
+    apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+})
+
 export default async function Dashboard({newIssue}: {newIssue: string}) {
-    const maintenanceData = await getMaintenanceData() as Issue[];
+    const maintenanceData = await client.fetch<Issue[]>(
+        `*[_type == "issue"]`,
+        {},
+        {
+          // You can set any of the `cache` and `next` options as you would on a standard `fetch` call
+          cache: 'force-cache',
+          next: {tags: ['pages']},
+        },
+      )
+    //const maintenanceData = await getMaintenanceData() as Issue[];
+    //console.log("data", maintenanceData);
+
     if (newIssue !== "" && newIssue !== undefined) {
         const convertedIssueToJSON = JSON.parse(newIssue);
         if (!isDuplicateIssue(convertedIssueToJSON, maintenanceData)) {
--- src/app/utils/getMaintenanceData.ts
+++ src/app/utils/getMaintenanceData.ts
@@ -1,5 +1,6 @@
 import {DummyIssues} from '../dashboard/api/dummyIssues';
 export default async function getMaintenanceData () {
+    console.log('hello', process.env.LOCAL_PROJECT_ID);
     return DummyIssues;
     // const response = await fetch('http://localhost:3000/dashboard/api');  
     // const data = await response.json();  
